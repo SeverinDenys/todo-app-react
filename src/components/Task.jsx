b5246f3fa79adcca263ad22de110360e377f35c9
Denys Severin <severindenys367@gmail.com>
2024-08-21 15:22:50 +0200

    first commit

---
+++ src/components/Task.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+
+function Task({ task, deleteTask }) {
+  const handleClick = () => {
+    deleteTask(task.id);
+  };
+  console.log(task);
+  return (
+    <div className="task-container">
+      <p className="task-container-text">{task.text}</p>
+      <div className="image-container">
+        <img
+          src="/images/icons-edit.png"
+          alt="Edit icon"
+          className="icon"
+        />
+        <img
+          src="/images/icons-delete.png"
+          alt="delete icon"
+          className="icon"
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Task;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+
+import Task from "./Task";
+function uuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(
+    /[018]/g,
+    (c) =>
+      (
+        +c ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] &
+          (15 >> (+c / 4)))
+      ).toString(16)
+  );
+}
+
+function Tasks() {
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]);
+  // const [newTask, setNewTask] = useState([]);
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTasks = [
+      ...tasks,
+      {
+        text: inputValue,
+        id: uuidv4(),
+      },
+    ];
+    setTasks(newTasks);
+    setInputValue("");
+  };
+  // console.log("tasks text", tasks[0].text);
+  const deleteTask = (idToRemove) => {
+    // 1 todo
+    // create new array based on tasks but without task id = id to remove
+    // 1 variant solution
+    setTasks(tasks.filter((task) => task.id !== idToRemove));
+
+    // 2 variant solution
+    // const newTaskList = tasks.filter(
+    //   (task) => task.id !== idToRemove
+    // );
+
+    // console.log(setTasks(newTaskList)); // why on delete it shows undefined?
+
+    // 2 todo
+    // set new state
+    // write const newTask that will save the value. ? Didn't understand the task
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="tasks">
+          <h1 className="tasks-title">Tasks Today</h1>
+        </div>
+        <div className="input">
+          <input
+            type="input"
+            value={inputValue}
+            onChange={handleChange}
+            className="input-value"
+            placeholder="add your task here"
+          />
+
+          <button
+            type="submit"
+            className="input-add"
+            onClick={handleSubmit}
+          >
+            +
+          </button>
+        </div>
+      </div>
+      {tasks.map((task, index) => (
+        <Task
+          key={index}
+          task={task}
+          deleteTask={deleteTask}
+          index={index}
+        />
+      ))}
+    </>
+  );
+}
+
+export default Tasks;
